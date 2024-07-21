3971b8ca9e36c9429c8a0ef205a47d4aee5b8e6d
David Adler <d.adler83@outlook.com>
2024-07-21 21:35:43 +0000

    dependenceis updated

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,6 +1,6 @@
-sudo apt-get install gcc-5-aarch64-linux-gnu g++-5-aarch64-linux-gnu gfortran-aarch64-linux-gnu
-sudo apt-get install gcc-5-arm-linux-gnueabi g++-5-arm-linux-gnueabi gfortran-arm-linux-gnueabi
-sudo apt-get install gnuplot
-sudo apt-get install g++ python-dev scons m4 swig
-sudo apt-get install zlib1g-dev
-sudo apt-get install device-tree-compiler
+sudo apt-get install gcc-5-aarch64-linux-gnu g++-5-aarch64-linux-gnu gfortran-aarch64-linux-gnu -y
+sudo apt-get install gcc-5-arm-linux-gnueabi g++-5-arm-linux-gnueabi gfortran-arm-linux-gnueabi -y
+sudo apt-get install gnuplot -y
+sudo apt-get install g++ python-dev python-pip scons m4 swig -y
+sudo apt-get install zlib1g-dev -y
+sudo apt-get install device-tree-compiler -y
